5a543f9f127e942cb22407ce0be3f2dc65048741
JOHAAAAAAAN <seba1sza@gmail.com>
2026-02-12 18:00:04 -0300

    Actualizar data.js

--- data.js
+++ data.js
@@ -40,6 +40,10 @@ const dataPartidos = [
     { mes: 2, jugador: "Isra", resultado: "V", goles: 2 },
     { mes: 2, jugador: "Mois", resultado: "V", goles: 0 },
     { mes: 2, jugador: "Seba", resultado: "V", goles: 4 },
+    
+    { mes: 2, jugador: "Isra", resultado: "D", goles: 2 },
+    { mes: 2, jugador: "Mois", resultado: "D", goles: 0 },
+    { mes: 2, jugador: "Seba", resultado: "D", goles: 4 },
 ];
 
 const jugadores = ["Isra", "Mois", "Seba"]; // Tus amigos
