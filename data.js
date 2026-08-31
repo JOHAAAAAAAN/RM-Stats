31ad2873180b5e5920bba4269618527278788030
JOHAAAAAAAN <seba1sza@gmail.com>
2026-08-31 10:27:30 -0400

    fulbo total

--- data.js
+++ data.js
@@ -212,6 +212,10 @@ const dataPartidos = [
     { mes: 8, jugador: "Mois", resultado: "D", goles: 1 },
     { mes: 8, jugador: "Seba", resultado: "D", goles: 0 },
 
+    { mes: 8, jugador: "Isra", resultado: "V", goles: 0 },
+    { mes: 8, jugador: "Mois", resultado: "V", goles: 1 },
+    { mes: 8, jugador: "Seba", resultado: "D", goles: 1 },
+
 
 
 
